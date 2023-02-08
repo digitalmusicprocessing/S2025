34bb34d0eb63b41ac215185c39929933e2c5ac4e
Chris Tralie <chris.tralie@gmail.com>
2023-02-08 15:43:42 -0500

    Hannah found typo

--- Assignments/HW3b_ConvolutionCompetition/Statements/convert.py
+++
@@ -1,9 +0,0 @@
-import glob
-import subprocess
-import os
-
-for f in glob.glob("*.wav"):
-    fout = f[0:-3] + "mp3"
-    if not os.path.exists(fout):
-        print(fout)
-        subprocess.call(["ffmpeg", "-i", f, fout])
--- Assignments/HW3b_ConvolutionCompetition/Statements/david/README.txt
+++
@@ -1,4 +0,0 @@
-Song Title: 
-	Drake: I'm On One in the Bathroom
-Description:
-	Took everything out of a bathroom to make the clap echo against the tile flooring.
